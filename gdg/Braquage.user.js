be8df4711575522927272991f4972844b02d0086
AurelienHarle <aurelienharle@hotmail.com>
2016-12-12 13:29:39 +0100

    Add dowload and update adresse

--- gdg/Braquage.user.js
+++ gdg/Braquage.user.js
@@ -2,6 +2,8 @@
 // @name        braquage
 // @namespace   Guerre des gangs
 // @include     https://www.guerredesgangs.net/fr/braquage.c
+// @downloadURL https://github.com/AurelienHarle/Script---GreaseMonkey/master/gdg/Braquage.user.js
+// @updateURL   https://github.com/AurelienHarle/Script---GreaseMonkey/master/gdg/Braquage.user.js
 // @version     1
 // @grant       none
 // ==/UserScript==
