45d5b4d5b44827d83002c091d1b89994b988da8b
AurelienHarle <aurelienharle@hotmail.com>
2016-12-12 13:29:36 +0100

    Add dowload and update adresse

--- gdg/Accueil.user.js
+++ gdg/Accueil.user.js
@@ -3,6 +3,8 @@
 // @namespace   Guerre des gangs
 // @description Renvoie sur les pages
 // @include     https://www.guerredesgangs.net/fr/home.c*
+// @downloadURL https://github.com/AurelienHarle/Script---GreaseMonkey/master/gdg/Accueil.user.js
+// @updateURL   https://github.com/AurelienHarle/Script---GreaseMonkey/master/gdg/Accueil.user.js
 // @version     1
 // @grant       none
 // ==/UserScript==
