b594fd44e41257e91cbcc6290921779af5804af4
AurelienHarle <aurelienharle@hotmail.com>
2016-12-14 11:48:25 +0100

    Ajout de la fonctionnalité de retrait

--- bazinio/banque.user.js
+++ bazinio/banque.user.js
@@ -1,11 +1,11 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name        banque
 // @namespace   Anarchy
 // @description depose en banque le surplus
-// @include     https://www.bazinio.com/banque.c
+// @include     https://www.bazinio.com/banque.c*
 // @downloadURL	https://github.com/AurelienHarle/Script---GreaseMonkey/master/bazinio/banque.user.js
 // @updateURL	https://github.com/AurelienHarle/Script---GreaseMonkey/master/bazinio/banque.user.js
-// @version     1
+// @version     1.01
 // @grant       none
 // ==/UserScript==
 
@@ -31,7 +31,47 @@ function depose(){
 	
 }
 	
+function retire(){
 	
+	if(document.getElementsByName('dep')[0] != undefined){
+		document.getElementsByName('dep')[0].click();
+	}
+	
+	inputRetire = document.getElementById('banque_retrait');
+	if(inputRetire == null){
+		setTimeout(changementCompte,randomTimeout());
+	}else{
+		btnRetire = document.getElementsByClassName('btn')[1].click();
+	}
+	
+	
+}	
+
+/**
+Change de compte
+*/
+function changementCompte(){
+	
+	/* Suivant la lite 1 10 100 101 peux etre trier*/
+	console.log("Time to swap master !");
+	
+	currentNumber = localStorage.number;
+	
+	if(currentNumber == undefined){
+		currentNumber = 1;
+		localStorage.number = 1;
+	}
+	
+	nextNumber = parseInt(currentNumber) + 1;
+	
+	if(nextNumber > 200){
+		nextNumber = 1;
+	}
+	
+	localStorage.number = nextNumber;
+	accountList = document.getElementById('chat').getElementsByTagName('a')[nextNumber].click();
+	
+}
 	
 /**
 Genere un chiffre aleatoire entre 750 et 2000, et est retourné pour generé un timeout aleatoire en milliseconde
@@ -45,4 +85,13 @@ function randomTimeout(){
 	return randTimeout;
 }
 
-setTimeout(depose,randomTimeout());
+
+if(localStorage.banque == "depose"){
+	
+	setTimeout(depose,randomTimeout());
+	
+}else if (localStorage.banque == "retire"){
+	
+	setTimeout(retire,randomTimeout());
+	
+}
