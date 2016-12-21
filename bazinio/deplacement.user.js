16b26fa49105a5a971caa4da1efb666bc54c5b14
AurelienHarle <aurelienharle@hotmail.com>
2016-12-21 12:16:03 +0100

    Retirer les depalcements des autres scripts

--- bazinio/deplacement.user.js
+++ bazinio/deplacement.user.js
@@ -4,12 +4,12 @@
 // @include     https://www.bazinio.com/*
 // @downloadURL	https://github.com/AurelienHarle/Script---GreaseMonkey/master/bazinio/deplacement.user.js
 // @updateURL	https://github.com/AurelienHarle/Script---GreaseMonkey/master/bazinio/deplacement.user.js
-// @version     1
+// @version     1.01
 // @grant       none
 // ==/UserScript==
 
 if(document.getElementsByName('dep')[0] != undefined){
 	
-		document.getElementsByName('dep')[0].click();
+	document.getElementsByName('dep')[0].click();
 		
 }
