0bfbea10bb094baf6fd923813bb4653e4dc633db
AurelienHarle <aurelienharle@hotmail.com>
2016-12-21 15:49:04 +0100

    Ajout documentation.

--- bazinio/sac.user.js
+++ bazinio/sac.user.js
@@ -4,38 +4,55 @@
 // @include     https://www.bazinio.com/garage.c
 // @downloadURL	https://github.com/AurelienHarle/Script---GreaseMonkey/master/bazinio/sac.user.js
 // @updateURL	https://github.com/AurelienHarle/Script---GreaseMonkey/master/bazinio/sac.user.js
-// @version     1
+// @version     1.01
 // @grant       none
 // ==/UserScript==
 
+/**
+Achete tout les sac disponibles
+*/
 function sac(){
 
 	sac = document.getElementsByName('new_car')[0];
+	
 	if(sac != undefined){
+		
 		sac = document.getElementsByName('new_car')[0].click();
+		
 	}else{
+		
 		setTimeout(changementCompte,randomTimeout);
+		
 	}
 }
 
 /**
-Change de compte
+Changement de compte
 */
 function changementCompte(){
 	
 	accountList = document.getElementById('chat').getElementsByTagName('a');
 	currentAccount = document.getElementsByClassName('lfiche')[0].getElementsByTagName('a')[0].innerHTML;
 	currentNumber = parseInt(currentAccount.split('e')[1]);
+	
 	if(currentNumber == 200){
+		
 		nextNumber = 1
+		
 	}else{
+		
 		nextNumber = currentNumber + 1;
+		
 	}
+	
 	nextAccount = "Suicide" + nextNumber;
+	
 	for(i = 0;i < accountList.length;i++){
 		
 		if(accountList[i].innerHTML ==  nextAccount){
+			
 			accountList[i].click();
+			
 		}
 	}
 }
@@ -45,11 +62,12 @@ Genere un chiffre aleatoire entre 750 et 2000, et est retourné pour generé un 
 */
 function randomTimeout(){
 	
-    var max = 2550;
-    var min = 950;
+	var max = 2550;
+	var min = 950;
     var randTimeout = min + (max-min+1) * Math.random();
     randTimeout = Math.trunc(randTimeout)
 	return randTimeout;
+	
 }
 
 setTimeout(sac,randomTimeout());
