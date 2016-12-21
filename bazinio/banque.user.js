c260365f6375e44a8f50967036c41a59540389e9
AurelienHarle <aurelienharle@hotmail.com>
2016-12-21 10:38:25 +0100

    Fix du CND

--- bazinio/banque.user.js
+++ bazinio/banque.user.js
@@ -1,169 +1,68 @@
 // ==UserScript==
-// @name        market
-// @namespace   anarchy
-// @description deal each hour
-// @include     https://www.bazinio.com/marche.c
-// @include     https://www.bazinio.com//login.php?id=*
-// @downloadURL	https://github.com/AurelienHarle/Script---GreaseMonkey/master/bazinio/market.user.js
-// @updateURL	https://github.com/AurelienHarle/Script---GreaseMonkey/master/bazinio/market.user.js
-// @version     1.01b
+// @name        banque
+// @namespace   Anarchy
+// @description depose en banque le surplus
+// @include     https://www.bazinio.com/banque.c*
+// @downloadURL	https://github.com/AurelienHarle/Script---GreaseMonkey/master/bazinio/banque.user.js
+// @updateURL	https://github.com/AurelienHarle/Script---GreaseMonkey/master/bazinio/banque.user.js
+// @version     1.01a
 // @grant       none
 // ==/UserScript==
 
-/**
-Create the market array for sell or buy
-*/
-function createDrug(){
-	
-	//console.log("Hey master I create the drug market for you");
-	
-	if(document.getElementsByName('dep')[0] != undefined){
-		document.getElementsByName('dep')[0].click();
-	}
+
+function depose(){
 	
 	cash = document.getElementsByClassName('liens')[2].getElementsByTagName('td')[0].innerHTML;
 	cash = cash.replace(/µ/,"");
 	cash = parseInt(cash.replace(/\s+/g,""));
 	
-	niveau = document.getElementsByClassName('lfiche')[1].innerHTML;
-	niveau = niveau.split('<')[0];
-	niveau = parseInt(niveau.replace(/N/g,""));
+	depose = cash - 2000000
+	if(depose == 0){
+		
+		document.getElementById('marche').click();
+		
+	}
+	document.getElementById('banque_depot').value = depose;
+	document.getElementsByClassName('btn')[0].click();
 	
-	if(cash > 30000000 && niveau > 1){
-		localStorage.banque = "cnd";
-		banque();
+}
+	
+function retire(){
+	
+	inputRetire = document.getElementById('banque_retrait');
+	if(inputRetire == null){
+		setTimeout(changementCompte,randomTimeout());
+	}else{
+		btnRetire = document.getElementsByClassName('btn')[1].click();
 	}
 	
-	market = document.getElementsByClassName('tableau market')[0].getElementsByTagName('tr');
 	
-	carriage = market[1].getElementsByTagName('th')[1].innerHTML;
-	carriageMax = carriage.split('/')[1];
-	carriage = carriage.split('/')[0];
+}	
+
+function cnd(){
 	
-	Drug = new Array();
+	cnd = document.getElementsByClassName('liens')[2].getElementsByTagName('td')[2].innerHTML;
+	cnd = cnd.replace(/µ/,"");
+	cnd = parseInt(cnd.replace(/\s+/g,""));
 	
-	for(i = 2,x = 0;i < market.length;i++,x++ ){
+	cash = document.getElementsByClassName('liens')[2].getElementsByTagName('td')[0].innerHTML;
+	cash = cash.replace(/µ/,"");
+	cash = parseInt(cash.replace(/\s+/g,""));
+	
+	cdnDepose = cash - 30000000
+	if(cash == 30000000){
 		
-		Drug[x] = new Object();
-		Drug[x].name = market[i].getElementsByTagName('td')[0].innerHTML;
+		document.getElementById('marche').click();
+		//setTimeout(changementCompte,randomTimeout());
 		
-		if(market[i].getElementsByTagName('td')[1].getElementsByTagName('img')[0] != undefined){
-			
-			Drug[x].price = market[i].getElementsByTagName('td')[1].getElementsByTagName('img')[0].title;
-			
-		}else{
-			
-			Drug[x].price = undefined;
-			
-		}
+	}else{
 		
-		if(market[i].getElementsByTagName('td')[3].getElementsByTagName('input')[2] != undefined && market[i].getElementsByTagName('td')[4] != undefined ){ //achat et vente
-			
-				
-				Drug[x].Vquantity = market[i].getElementsByTagName('td')[4].getElementsByTagName('input')[1];
-				Drug[x].Aquantity = market[i].getElementsByTagName('td')[3].getElementsByTagName('input')[1];
-				Drug[x].Vboutton = market[i].getElementsByTagName('td')[4].getElementsByTagName('input')[2];
-				Drug[x].Aboutton = market[i].getElementsByTagName('td')[3].getElementsByTagName('input')[2];
-				
-				Drug[x].action = "vendre/acheter";
-						
-		}else if(market[i].getElementsByTagName('td')[3].getElementsByTagName('input')[2] != undefined && market[i].getElementsByTagName('td')[4] == undefined){ //achat
-				
-				Drug[x].quantity = market[i].getElementsByTagName('td')[3].getElementsByTagName('input')[1];
-				Drug[x].boutton = market[i].getElementsByTagName('td')[3].getElementsByTagName('input')[2];
-				
-				Drug[x].action = "acheter";
-		
-		}else if(market[i].getElementsByTagName('td')[3].getElementsByTagName('input')[2] == undefined && market[i].getElementsByTagName('td')[4] != undefined){ //vente
-		
-			Drug[x].quantity = market[i].getElementsByTagName('td')[4].getElementsByTagName('input')[1];
-			Drug[x].boutton = market[i].getElementsByTagName('td')[4].getElementsByTagName('input')[2];
-			Drug[x].action = "vendre";
-			
-		}
+		document.getElementById('magot_depot').value = cdnDepose;
+		document.getElementsByClassName('btn')[1].click();
 		
 	}
 	
-	//console.log("Wow all this drug look amasing let's go to do something with it");
-	setTimeout(achatVente,1000);
 	
-}
-
-function achatVente(){
-	
-	//console.log("let me check what we need to do with each drug");
-	
-	for(i = 0;i < Drug.length;i++){
-		if(Drug[i].action == "vendre/acheter"){
-			
-			if(Drug[i].price <= 4){
-				
-				//console.log("Great price let me buy !");
-				Drug[i].boutton = Drug[i].Aboutton;
-				buy();
-				break;
-				
-			}
-			
-			if(Drug[i].price >= 5){
-				
-				//console.log("Great price let me sell !");
-				Drug[i].boutton = Drug[i].Vboutton;
-				sell();
-				break;
-			}
-	
-		}else if(Drug[i].action == "acheter"){
-			//console.log("let me look if the price are ok for buy");
-			if(Drug[i].price <= 4){
-				
-				//console.log("Great price let me buy !");
-				buy();
-				break;
-			}
-			
-		}else if(Drug[i].action == "vendre"){
-			//console.log("let me look if the price are ok for sell");
-			if(Drug[i].price >= 5){
-				
-				//console.log("Great price let me sell !");
-				sell();
-				break;
-			}
-			
-		}else{
-			
-			//console.log("Nothing to do :(");
-			
-		}
-		
-	}
-	
-	setTimeout(changementCompte,randomTimeout());
-}
-
-
-/**
-Vente du marcher
-*/
-function sell(){
-	
-	//console.log("Click for sell!");
-	
-	Drug[i].boutton.click();
-	
-	//setTimeout(cooldown,1000);
-}
-
-/**
-Achat du marcher
-*/
-function buy(){
-	
-	//console.log("Click for buy!");
-	
-	Drug[i].boutton.click();
-	//setTimeout(cooldown,1000);
 }
 
 /**
@@ -186,26 +85,31 @@ function changementCompte(){
 			accountList[i].click();
 		}
 	}
-	
 }
-
+	
 /**
 Genere un chiffre aleatoire entre 750 et 2000, et est retourné pour generé un timeout aleatoire en milliseconde
 */
 function randomTimeout(){
 	
-	var max = 1000;
-	var min = 250;
+	var max = 2550;
+	var min = 950;
     var randTimeout = min + (max-min+1) * Math.random();
     randTimeout = Math.trunc(randTimeout)
 	return randTimeout;
 }
 
-function banque(){
+
+if(localStorage.banque == "depose"){
 	
-	document.getElementById('banque').click();
+	setTimeout(depose,randomTimeout());
+	
+}else if (localStorage.banque == "retire"){
+	
+	setTimeout(retire,randomTimeout());
+	
+}else if (localStorage.banque == "cnd"){
+	
+	setTimeout(cnd,randomTimeout());
 	
 }
-
-
-setTimeout(createDrug,randomTimeout());
