1353ad2e69d27263d78a0ebf0779670467b58ca5
AurelienHarle <aurelienharle@hotmail.com>
2017-01-03 09:10:19 +0100

    Ajout du controle du cash

--- bazinio/heal.user.js
+++ bazinio/heal.user.js
@@ -3,14 +3,18 @@
 // @namespace   bazinio
 // @include     https://www.bazinio.com/*
 // @exclude     https://www.bazinio.com/medecin.c
-// @version     1
+// @version     1.01
 // @grant       none
 // ==/UserScript==
 
 vie = document.getElementById('col_fiche').getElementsByTagName('td')[0].innerHTML;
 vie = parseInt(vie.split('/')[0]);
+
+cash = document.getElementsByClassName('liens')[2].getElementsByTagName('td')[0].innerHTML;
+	cash = cash.replace(/µ/,"");
+	cash = parseInt(cash.replace(/\s+/g,""));
 	
-if(vie < 50){
+if(vie < 50 && cash > 2000000){
 	
 	document.getElementById('medecin').click();
 	
