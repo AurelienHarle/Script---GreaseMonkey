750e8d7e9fb5a67179f32bd3c1d693f9eabe40d7
Harlé <aurelienharle@hotmail.com>
2017-01-23 12:04:13 +0100

    premiere update
    manque, la fonction bouton > image

---
+++ Utilitaire/LowConnection.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name        LowConnection
+// @namespace   Utilitaire
+// @include     https://*
+// @include     http://*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+/**
+	Recupere toute les sources des image
+*/
+function imageToButton(){
+	
+	tabImage = document.getElementsByTagName('img');
+	size = tabImage.length;
+	for(var i = 0;i < size;i++){
+		
+		parentImage = tabImage[i].parentElement;
+		
+		largeur = tabImage[i].width;
+		hauteur = tabImage[i].height;
+		
+		newElement = document.createElement('input');
+		newElement.value = "Charger l'image...";
+		newElement.type = "button";
+		newElement.width = largeur;
+		newElement.height = hauteur;
+		
+		newTextNode = document.createTextNode("Charger l'image");
+		newElement.appendChild(newTextNode);
+		
+		newElement.src = tabImage[i].src;
+		newElement.addEventListener('click',buttonToImage);
+		
+		parentImage.replaceChild(newElement,tabImage[i]);
+		i--;
+	}
+}
+
+/**
+	Retransforme une image si on appuys sur le  boutton qui la remplace
+*/
+function buttonToImage(){
+	
+	console.log("Click click")
+	
+}
+
+imageToButton();
